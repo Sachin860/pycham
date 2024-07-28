423c902b473bef4b57469735bbd34832f101daa5
Sachin860 <sachinchandrasekar860@gmail.com>
2024-07-28 19:39:53 +0530

    sql_completed

--- sql_project.py
+++ sql_project.py
@@ -1,3 +1,4 @@
+from typing import List
 # import pandas as pd
 # import sqlalchemy as sa
 # class Sql:
@@ -54,44 +55,68 @@
 #     if choice==2:
 #         print('invalid data')
 #         break
-
+from typing import List
 import pandas as pd
 import pyodbc
 class Sql:
-    def __init__(self,server,database,user_name,password):
-        self.connection=pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};'
-                                f'SERVER={server};'
-                                f'DATABASE={database};'
-                                f'UID={user_name};'
-                                f'PWD={password}')
-    def show(self,comment):
-        cursor=self.connection.cursor()
+    def __init__(self, server, database, user_name, password):
+        self.connection = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};'
+                                         f'SERVER={server};'
+                                         f'DATABASE={database};'
+                                         f'UID={user_name};'
+                                         f'PWD={password}')
+
+    def show(self, comment):
+        cursor = self.connection.cursor()
         cursor.execute(comment)
-        dataframe=cursor.fetchall()
+        dataframe = cursor.fetchall()
         for data in dataframe:
-            print(data)
-def menu():
+            df = (list(data))
+            print(df)
+
+
+def menu1():
     print('create your own SQL Database ')
     print('press 1 to connect your sql server and database')
     print('press 2 to exit')
 
+
+def menu2():
+    print('to insert your data in table press 1')
+    print('to select a table press 2')
+    print('to update a your name or any press 3')
+    print('to delete any data or cell press 4')
+
+
 while True:
-    print(menu())
-    choice=int(input('enter your choice'))
-    if choice==1:
-        server=input('enter your name of your Sql sever EX://KEERTHANA\MSSQLSERVER01')
-        database=input('select your database')
-        user_name=input('enter your user_name')
-        password=input('enter your SQl sever password')
-        comment=input('enter your comment')
-        sachin = Sql(server,database,user_name,password)
-        sachin.show(comment)
+    print(menu1())
+    choice = int(input('enter your choice'))
+    if choice == 1:
+        server = input('enter your name of your SQL sever EX://- KEERTHANA\MSSQLSERVER01')
+        database = input('select your database - sachin')
+        user_name = input('enter your user_name - sa')
+        password = input('enter your SQl server password - Sachin$00')
+        sachin = Sql(server, database, user_name, password)
+        print('sever connected')
+        while True:
+            print(menu2())
+            choice = int(input('enter your choice'))
+            if choice == 1:
+                name = input('enter a name')
+                age = int(input('enter your age'))
+                mail = input('enter your mailid')
+                cursor = sachin.connection.cursor()
+                # cursor.execute(f'insert into studentdata (id,name,age,mail) values ({name},{age},{mail})')
+                cursor.execute('INSERT INTO studentdata (name, age, mail) VALUES (?, ?, ?)', (name, age, mail))
+                sachin.connection.commit()
+                print('data inserted in database')
+                break
+    elif choice == 2:
+        print('Exited')
         break
-    if choice==2:
-        print('invalid data')
+    else:
+        print('make a correct input')
         break
-
-
 
 
 
